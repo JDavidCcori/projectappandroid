8361eca187bec65fc838b603bb2c9a873a5dec7f
JDavidCcori <datatheyvit123@gmail.com>
2023-07-18 14:38:02 -0500

    add credentials of firestore

---
+++ src/app/credentials.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  useEmulators: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDL2mCsY8gBNnOkqZPx6-pLFoxdNvUjQ1s',
+    authDomain: 'newappdavid-b17e8.firebaseapp.com',
+    projectId: 'newappdavid-b17e8',
+    storageBucket: 'newappdavid-b17e8.appspot.com',
+    messagingSenderId: '240508205012',
+    appId: '1:240508205012:web:8c9418f2936a522975c841',
+    measurementId: 'G-FW1PD6EGZV',
+  },
+};
