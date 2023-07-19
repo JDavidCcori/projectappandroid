8aa4745f60aa57bb952a249b8bf9edcdc45f7211
JDavidCcori <datatheyvit123@gmail.com>
2023-07-19 02:20:55 -0500

    modified view login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { Auth } from '@angular/fire/auth';
 
 
 @Component({
@@ -11,6 +12,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  formLogin: FormGroup;
   credentials = {
     correo: '',
     password: ''
@@ -19,17 +21,38 @@ export class LoginPage implements OnInit {
     private auth: AuthService,
     private router: Router,
     private menu: MenuController,
-  ) { }
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+   }
 
   ngOnInit() {
   }
 
-  async login() {
-    const res = await this.auth.login(this.credentials.correo, this.credentials.password).catch( error => {
-        console.log('error');
-    })
-    if (res) {
-        this.router.navigate(['/home'])
-    }
+  onSubmit() {
+    this.auth.register(this.formLogin.value)
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['/home']);
+      })
+      .catch(error => console.log(error));
+  }
+
+  onClick() {
+    this.auth.loginWithGoogle()
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['/home']);
+      })
+      .catch(error => console.log(error))
+  }
+
+  loginF(){
+    this.auth.loginWithFacebook().then(res => {
+      console.log(res);
+      this.router.navigate(['/home']);
+    }).catch(error=> console.log(error))
   }
 }
